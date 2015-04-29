1c66829a115ce048e34d59da0e72a309c0118768
rtubiopa@calpoly.edu <rtubiopa@calpoly.edu>
2015-04-28 22:29:52 -0700

    jquery and angular-material moved to node_modules

--- karma.conf.js
+++ karma.conf.js
@@ -7,10 +7,10 @@ module.exports = function (config) {
         frameworks: ['jasmine'],
 
         files: [
-            'lib/bower/jquery/dist/jquery.js',
+            'node_modules/jquery/dist/jquery.js',
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
-            'lib/bower/angular-material/angular-material.js',
+            'node_modules/angular-material/angular-material.js',
             'src/scripts/services/celestrak.js',
             'src/scripts/services/broadcaster.js',
             'src/scripts/services/satnet.js',
