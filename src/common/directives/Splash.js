4c4077b15613fefe80cc423f4386f5c4d06dc67e
Ricardo Tubio Pardavila <rtubiopa@calpoly.edu>
2015-05-09 14:13:38 -0700

    (backup)

--- src/common/directives/Splash.js
+++ src/common/directives/Splash.js
@@ -14,7 +14,7 @@
    limitations under the License.
 */
 
-angular.module('splashDirective', ['ngAnimate'])
+angular.module('splashDirective', [])
     .directive('mAppLoading', ['$animate',
 
     /**
