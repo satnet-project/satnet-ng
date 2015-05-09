0a1ce8ca42f03178e3e8a71a71a5ece64443d6d9
Ricardo Tubio Pardavila <rtubiopa@calpoly.edu>
2015-05-09 11:52:44 -0700

    (backup)

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,19 @@
+/*
+   Copyright 2014 Ricardo Tubio-Pardavila
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 module.exports = function (config) {
     'use strict';
 
@@ -39,12 +55,14 @@ module.exports = function (config) {
                 return filepath.replace(regex, '');
             },
         },
+
         reporters: ['progress', 'coverage', 'coveralls'],
         coverageReporter: {
             type: 'lcov',
             repoToken: '',
             dir: '.coverage/'
         },
+
         port: 9876,
         colors: true,
         logLevel: config.LOG_DEBUG,
