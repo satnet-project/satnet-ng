53717e6038e1a1d28ab580c8c89056c27706a7e4
rtubiopa@calpoly.edu <rtubiopa@calpoly.edu>
2015-05-08 23:35:26 -0700

    (backup)

---
+++ src/common/specs/services/Pusher.spec.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2014 Ricardo Tubio-Pardavila
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by rtubio on 10/24/14.
+ */
+
+describe('Testing Pusher Service', function () {
+
+    var $rootScope, pushServices, mock__pusher = {};
+
+    beforeEach(function () {
+        module('pushServices');
+
+        module(function ($provide) {
+            $provide.value('$pusher', mock__pusher);
+        });
+
+        inject(function ($injector) {
+            //pushServices = $injector.get('satnetPush');
+            $rootScope = $injector.get('$rootScope');
+        });
+
+    });
+
+    it('should return a non-null pushServices object', function () {
+        expect(pushServices).not.toBeNull();
+    });
+
+});
