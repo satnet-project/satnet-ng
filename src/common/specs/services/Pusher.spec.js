2f66a38982c8f5ee11b45df6d4d1c49d5f84a101
Ricardo Tubio Pardavila <rtubiopa@calpoly.edu>
2015-05-14 16:39:12 -0700

    (backup)

--- src/common/specs/services/Pusher.spec.js
+++ src/common/specs/services/Pusher.spec.js
@@ -63,14 +63,43 @@ describe('Testing Pusher Service', function () {
 
     });
 
-    it('should return a non-null pushServices object', function () {
+    it('should return a valid API pushServices object', function () {
         expect(satnetPush).toBeDefined();
-    });
 
+        expect(satnetPush.LEOP_DOWNLINK_CHANNEL).toBe('leop.downlink.ch');
+        expect(satnetPush.EVENTS_CHANNEL).toBe('configuration.events.ch');
+        expect(satnetPush.NETWORK_EVENTS_CHANNEL).toBe('network.events.ch');
+        expect(satnetPush.SIMULATION_CHANNEL).toBe('simulation.events.ch');
+        expect(satnetPush.LEOP_CHANNEL).toBe('leop.events.ch');
+
+        expect(satnetPush.KEEP_ALIVE).toBe('keep_alive');
+        expect(satnetPush.FRAME_EVENT).toBe('frameEv');
+
+        expect(satnetPush.GS_ADDED_EVENT).toBe('gsAddedEv');
+        expect(satnetPush.GS_REMOVED_EVENT).toBe('gsRemovedEv');
+        expect(satnetPush.GS_UPDATED_EVENT).toBe('gsUpdatedEv');
+
+        expect(satnetPush.PASSES_UPDATED_EVENT).toBe('passesUpdatedEv');
+        expect(satnetPush.GT_UPDATED_EVENT).toBe('groundtrackUpdatedEv');
+
+        expect(satnetPush.LEOP_GSS_UPDATED_EVENT).toBe('leopGSsUpdatedEv');
+        expect(satnetPush.LEOP_GS_ASSIGNED_EVENT).toBe('leopGSAssignedEv');
+        expect(satnetPush.LEOP_GS_RELEASED_EVENT).toBe('leopGSReleasedEv');
+
+        expect(satnetPush.LEOP_UPDATED_EVENT).toBe('leopUpdatedEv');
+
+        expect(satnetPush.LEOP_UFO_IDENTIFIED_EVENT).toBe('leopUFOIdentifiedEv');
+        expect(satnetPush.LEOP_UFO_FORGOTTEN_EVENT).toBe('leopUFOForgottenEv');
+        expect(satnetPush.LEOP_SC_UPDATED_EVENT).toBe('leopSCUpdatedEv');
+
+    });
+    
     /* FIXME: $pusher.allChannels() is undefined, ask @github?
     it('it should bind callbacks to the channels', function () {
 
         var __mock__frame_rx_cb = function () {};
+
+        expect(satnetPush.bind('xxxx', 'zzzz', function () {})).toThrow('Not subscribed to this channel, name = xxxx');
 
         satnetPush.bind(
             'leop.downlink.ch', 'frameEv', __mock__frame_rx_cb, satnetPush
